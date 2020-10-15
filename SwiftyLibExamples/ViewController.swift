5c786b4932c6f74850ef9533e7b35574968f7ca5
Muzammil-iOS <72791019+Muzammil-iOS@users.noreply.github.com>
2020-10-15 12:26:54 +0500

    first commit

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Muzammil on 15/10/2020.
+//  Copyright © 2020 Muzammil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
