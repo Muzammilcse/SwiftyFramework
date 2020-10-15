d58f05d334c5c0f7aea800bcf7ea086c2b8688ae
Muzammil-iOS <72791019+Muzammil-iOS@users.noreply.github.com>
2020-10-15 12:41:21 +0500

    Add test cases

---
+++ SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Muzammil on 15/10/2020.
+//  Copyright © 2020 Muzammil. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
